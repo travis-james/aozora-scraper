840d76f83bfe60bb6dd9e47a5e1cc12fb7febf80
travis <travis.hend@gmail.com>
2021-01-14 22:00:56 -0800

    thinking

--- main.go
+++ main.go
@@ -11,15 +11,17 @@ import (
 * use http.Get(URL) to get bytes of file from url
 * create an empty file using os.Create
 * use io.Copy to copy downloaded bytes to file created.
+*
+* unzip -O shift-jis fire.zip
  */
 func main() {
-	resp, err := http.Get("https://www.aozora.gr.jp/cards/000035/files/1578_44923.html")
+	resp, err := http.Get("https://www.aozora.gr.jp/index_pages/person20.html")
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 
-	img, err := os.Create("story.html")
+	img, err := os.Create("test3.html")
 	if err != nil {
 		panic(err)
 	}
